9d7e075e3e6ceef37a1566bfc51b33556cd2a998
Zachary Malinka <zacharymalinka@gmail.com>
2025-03-04 20:46:45 -0500

    Added reviews and comments on singleAudio. Can also create a review for singleAudio. Comments are janked on singleAudio. Move comments to a seperate component. I created a single review component. Move them there.

--- frontend/components/AudioCreator/AudioCreatorApi.js
+++ frontend/components/AudioCreator/AudioCreatorApi.js
@@ -1,21 +1,15 @@
 export const triggerBackend = async () => {
   try {
-    // const response = await fetch("http://127.0.0.1:4000/start", {
     //this is for local
+    // const response = await fetch("http://127.0.0.1:4000/start", {
     const response = await fetch(
       "https://s1w15x1qoc.execute-api.us-east-2.amazonaws.com/test",
       {
-        // This is for deployment
         method: "POST",
-        // POST for deployment
         headers: {
           "Content-type": "application/json",
-          // "Access-Control-Allow-Origin": "*",
-          // "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
-          // "Access-Control-Allow-Headers": "Content-Type",
-          // I don't think we need cors for deploying 
         },
-        body: JSON.stringify({ message: "User would like to start" }), // body for POST deployment
+        body: JSON.stringify({ message: "User would like to start" }),
       }
     );
     const data = await response.json();
